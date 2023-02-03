f11069dff79566b9ea61d04d8c7add97214185d8
sujin lim <swuujin990827@gmail.com>
2023-02-03 10:51:41 +0900

    feat: 로그인, 회원가입 dto

--- src/main/java/com/hello/shop/dto/SignupDto.java
+++ src/main/java/com/hello/shop/dto/SignupDto.java
@@ -1,30 +1,31 @@
 package com.hello.shop.dto;
-
-import com.hello.shop.domain.User;
+import com.hello.shop.domain.user.User;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class SignupDto {
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private String address;
+    private String phone;
+    private String role;
 
-	private String username;
-	private String password;
-	private String email;
-	private String name;
-	private String address;
-	private String phone;
-	private String role;
-
-	public User toEntity(){
-		return User.builder()
-			.username(username)
-			.password(password)
-			.email(email)
-			.name(name)
-			.address(address)
-			.phone(phone)
-			.role(role)
-			.build();
-	}
-
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .name(name)
+                .address(address)
+                .phone(phone)
+                .role(role)
+                .build();
+    }
 }
